403c32bdfbbf4999aef8702c49fcb9863550f06b
Prawira Genestonlia <prawira.96@gmail.com>
2019-10-21 16:06:57 +0800

    add authorization

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const auth = require('basic-auth');
 const path = require('path');
 const app = express();
 app.disable('x-powered-by');
+
+const admins = { sldu: { password: 'sldus2b2a39' } };
+function authorization(request, response, next) {
+  var user = auth(request);
+  if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+    response.set('WWW-Authenticate', 'Basic realm="slduwebapp"');
+    return response.status(401).send("Unauthorized user");
+  }
+  return next();
+}
+app.use(authorization);
+
 app.use(express.static(path.join(__dirname, '../slduwebapp/build')));
 // need to declare a "catch all" route on your express server 
 // that captures all page requests and directs them to the client
@@ -11,7 +23,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../slduwebapp/build', 'index.html'));
 });
 app.listen(
-  process.env.PORT || 3820,
+  process.env.PORT || 3500,
   function () {
     console.log(`Frontend start on http://localhost:3820`)
   }
